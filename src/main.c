e5083941c4b450bd043168ff7a280daf6e6333d6
Olivia Seow <oliviaseow@C02D50Z1MD6M.tld>
2020-12-30 20:36:26 -0800

    3d to 2d projection

--- src/main.c
+++ src/main.c
@@ -3,8 +3,23 @@
 #include <stdbool.h>
 #include <SDL2/SDL.h>
 #include "display.h"
+#include "vector.h"
+
+#define N_POINTS (9*9*9)
+
+//const int N_POINTS = 9*9*9
+vec3_t cube_points[N_POINTS];
+vec2_t projected_points[N_POINTS];
+
+float fov_factor = 128;
 
 bool is_running = false;
+
+typedef struct {
+	vec3_t position;
+	vec3_t rotation;
+	float fov_angle;
+} camera_t;
 
 void setup(void) {
 	// Allocate the required bytes in memory for the color buffer
@@ -21,10 +36,17 @@ void setup(void) {
 		window_height
 	);
 
-	// if (!color_buffer_texture) {
-	// 	fprintf(stderr, "Error creating SDL texture.\n");
-	// 	return false;
-	// }
+	int point_count = 0;
+
+	// Start loading an array of vectors from -1 to 1 (in this 9 by 9 by 9 cube)
+	for (float x = -1; x <= 1; x += 0.25) {
+		for (float y = -1; y <= 1; y += 0.25) {
+			for (float z = -1; z <= 1; z += 0.25) {
+				vec3_t new_point = {.x = x, .y = y, .z = z};
+				cube_points[point_count++] = new_point;
+			}
+		}
+	}
 }
 
 void process_input(void) {
@@ -42,20 +64,50 @@ void process_input(void) {
 	}
 }
 
+vec2_t project(vec3_t point) {
+	//convert 3D vector to projected 2D point
+	vec2_t projected_point = {
+		.x = fov_factor * point.x,
+		.y = fov_factor * point.y
+	};
+	return projected_point;
+}
+
 
 void update(void) {
+	for (int i = 0; i < N_POINTS; i++) {
+		vec3_t point = cube_points[i];
 
+		//project the current point
+		vec2_t projected_point = project(point);
+
+		//save the projected 2D vector in the array of projected points
+		projected_points[i] = projected_point;
+	}
 }
 
 void render(void) {
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-	SDL_RenderClear(renderer);
+	// we don't need these anymore as we are using the color buffer
+	// SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	// SDL_RenderClear(renderer);
 
 	draw_grid();
 
-	draw_rect(300, 200, 300, 150, 0xFFFF00FF);
+	// draw_pixel(50, 50, 0xFFFFFF00);
+	// draw_rect(300, 200, 300, 150, 0xFFFF00FF);
+
+	for (int i = 0; i < N_POINTS; i++) {
+		vec2_t projected_point = projected_points[i];
+		draw_rect(
+			projected_point.x + (window_width/2),
+			projected_point.y + (window_height/2),
+			4,
+			4,
+			0xFFFFFF00);
+	}
 
 	render_color_buffer();
+
 	clear_color_buffer(0xFF000000);
 
 	SDL_RenderPresent(renderer);
@@ -66,6 +118,14 @@ int main(void) {
 	is_running = initialize_window();
 
 	setup();
+
+	//vec3_t myvector = {2.0, 3.0, -4.0};
+
+	camera_t mycam = {
+		{ 0, 0, 0},
+		{ 0.3, -2.0, 0.0 },
+		0.78
+	};
 
 	while (is_running) {
 		process_input();
---
+++ src/vector.h
@@ -0,0 +1,18 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+//declare a new type to hold 2d vectors
+typedef struct {
+	float x;
+	float y;
+} vec2_t;
+
+typedef struct {
+	float x;
+	float y;
+	float z;
+} vec3_t;
+
+
+
+#endif
