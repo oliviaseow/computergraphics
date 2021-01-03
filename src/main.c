1bcd7c0aa0819f35e676352a83970117cb8a7bb1
Olivia Seow <oliviaseow@C02D50Z1MD6M.tld>
2021-01-03 08:34:33 -0800

    sort faces with painter's algorithm

--- src/main.c
+++ src/main.c
@@ -219,12 +219,16 @@ void update(void) {
 			projected_points[j].y += (window_height / 2);
 		}
 
+		// calculate the average depth for each face based on the vertices after transformation
+		float avg_depth = (transformed_vertices[0].z + transformed_vertices[1].z + transformed_vertices[2].z)/3;
+
 		triangle_t projected_triangle = {
 			.points = {
 				{ projected_points[0].x, projected_points[0].y },
 				{ projected_points[1].x, projected_points[1].y },
 				{ projected_points[2].x, projected_points[2].y }},
-			.color = mesh_face.color};			
+			.color = mesh_face.color,
+			.avg_depth = avg_depth};			
 
 			//projected_triangle.points[j] = projected_point;
 		
@@ -232,6 +236,20 @@ void update(void) {
 		//save projected triangle in array of triangles to render
 		//triangles_to_render[i] = projected_triangle;
 		array_push(triangles_to_render, projected_triangle);
+	}
+
+	// Sort triangles to render by their avg_depth
+	int num_triangles = array_length(triangles_to_render);
+
+	for (int i = 0; i < num_triangles; i++) {
+		for (int j = i; j < num_triangles; j++) {
+			if (triangles_to_render[i].avg_depth < triangles_to_render[j].avg_depth) {
+				// swap the triangles position in the array (bubble sort)
+				triangle_t temp = triangles_to_render[i];
+				triangles_to_render[i] = triangles_to_render[j];
+				triangles_to_render[j] = temp;
+			}
+		}
 	}
 }
 
