50e20e465fab5707ea5b823539eb9159fae3dce6
Olivia Seow <oliviaseow@C02D50Z1MD6M.tld>
2021-01-03 07:49:23 -0800

    avoid division by zero

--- src/triangle.c
+++ src/triangle.c
@@ -109,14 +109,25 @@ void draw_filled_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32
 		int_swap(&x0, &x1);
 	}
 
-	// calculate new midpoint vertex (<x, My) using triangle similarity
-	int My = y1;
-	int Mx = ((float)((x2 - x0) * (y1 - y0))/(float)(y2 - y0)) + x0;
+	// avoid division by zero
+	if (y1 == y2) {
+		fill_flat_bottom_triangle(x0, y0, x1, y1, x2, y2, color);
+	} else if (y0 == y1) {
+		fill_flat_top_triangle(x0, y0, x1, y1, x2, y2, color);
+	} else {
+		// calculate new midpoint vertex (<x, My) using triangle similarity
+		int My = y1;
+		int Mx = ((float)((x2 - x0) * (y1 - y0))/(float)(y2 - y0)) + x0;
 
-	//draw flat-bottom triangle
-	fill_flat_bottom_triangle(x0, y0, x1, y1, Mx, My, 0xFF00FF00);
+		//draw flat-bottom triangle
+		fill_flat_bottom_triangle(x0, y0, x1, y1, Mx, My, color);
 
-	//draw flat-top triangle
-	fill_flat_top_triangle(x1, y1, Mx, My, x2, y2, 0xFF00FF00);
+		//draw flat-top triangle
+		fill_flat_top_triangle(x1, y1, Mx, My, x2, y2, color);
+
+	}
+
+
+
 
 }
